ab9d3f0d2190f2a8425c80f83e0f02f2cc9f64ad
Francis <fransk374@gmail.com>
2025-04-26 04:17:47 -0500

    archivo imprimir

---
+++ imprimir.py
@@ -0,0 +1,11 @@
+a = "Carlos"
+b=20
+print("Hola", a, "tienes", b)
+
+c = "Hola " + str(a) + " tienes " + str(b)
+print(c)
+
+print("Carlos", "Jose", "Juan", sep="- ", end="#")
+"""
+COMENTARIOS VARIAS LINEAS
+"""
---
+++ program.py
@@ -0,0 +1,7 @@
+a = 2**3
+
+if a==8:
+    print(a)
+else:
+    print("No")
+    
